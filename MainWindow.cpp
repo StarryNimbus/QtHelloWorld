220dfb6cfa2ebc4083f54221024ec111ecd88188
Starry Nimbus <starrynimbus01000@gmail.com>
2026-01-05 22:09:50 -0800

    Encapsulate widget creation in member functions

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,59 +1,30 @@
 #include "MainWindow.h"
 #include <QDebug>
-#include <QLabel>
 #include <QMenuBar>
 #include <QStatusBar>
 #include <QVBoxLayout>
 
 MainWindow::MainWindow(bool isDarkMode, QWidget *parent)
-    : QMainWindow(parent), m_isDarkMode(isDarkMode) {
+    : QMainWindow(parent), m_isDarkMode(isDarkMode),
+      m_textColor(m_isDarkMode ? "white" : "black") {
   qDebug() << "Dark Mode: " << isDarkMode;
 
   setWindowTitle("QtHelloWorld");
   setGeometry(0, 0, 800, 800);
 
-  // Example menu bar
-  // QT behaves differently on Mac than on Windows/Linux when it comes to menu
-  // bars. Toggle setNativeMenuBar depending on OS.
-  QMenuBar *menuBar = new QMenuBar(this);
-  QMenu *fileMenu = menuBar->addMenu("File");
-  fileMenu->addAction("Exit", this, &QMainWindow::close);
-  menuBar->setNativeMenuBar(false);
+  // Top of app
+  QMenuBar *menuBar = CreateMenuBar();
   setMenuBar(menuBar);
 
-  const QString textColor = m_isDarkMode ? "white" : "black";
-
-  // Example "Hello World" label
-  QLabel *label = new QLabel("Hello World!", this);
-  label->setAlignment(Qt::AlignCenter);
-  label->setStyleSheet(QString("QLabel {"
-                               "  font-size: 28px;"
-                               "  font-weight: bold;"
-                               "  color: %1;"
-                               "}")
-                           .arg(textColor));
-
-  // Example "Hello World" sublabel
-  QLabel *subLabel = new QLabel("Est. 2026", this);
-  subLabel->setAlignment(Qt::AlignCenter);
-  subLabel->setStyleSheet(QString("QLabel {"
-                                  "  font-size: 11px;"
-                                  "  font-weight: normal;"
-                                  "  color: %1;"
-                                  "}")
-                              .arg(textColor));
-
-  // Example "Hello World" image
-  QLabel *image = new QLabel(this);
-  QPixmap pixmap(":/images/globe.png");
-  pixmap =
-      pixmap.scaled(250, 250, Qt::KeepAspectRatio, Qt::SmoothTransformation);
-  image->setPixmap(pixmap);
-  image->setFixedSize(250, 250);
-
-  // Central widget to encapsulate the verticalLayout
+  // App content
+  // Central widget to encapsulate a verticalLayout which contains the title,
+  // subtitle, and image
   QWidget *centralWidget = new QWidget(this);
   setCentralWidget(centralWidget);
+
+  QLabel *label = CreateTitle();
+  QLabel *subLabel = CreateSubtitle();
+  QLabel *image = CreateCenterImage();
 
   QVBoxLayout *verticalLayout = new QVBoxLayout(centralWidget);
   verticalLayout->setAlignment(Qt::AlignCenter);
@@ -63,8 +34,52 @@ MainWindow::MainWindow(bool isDarkMode, QWidget *parent)
   verticalLayout->addWidget(subLabel);
   verticalLayout->addStretch();
 
-  // Example status bar
+  // Bottom of app
   QStatusBar *statusBar = new QStatusBar(this);
-  statusBar->showMessage("Ready");
+  statusBar->showMessage(QString("Dark Mode: %1").arg(m_isDarkMode), 0);
   setStatusBar(statusBar);
+}
+
+QMenuBar *MainWindow::CreateMenuBar() {
+  // QT behaves differently on Mac than on Windows/Linux when it comes to menu
+  // bars. Toggle setNativeMenuBar depending on OS.
+  QMenuBar *menuBar = new QMenuBar(this);
+  QMenu *fileMenu = menuBar->addMenu("File");
+  fileMenu->addAction("Exit", this, &QMainWindow::close);
+  menuBar->setNativeMenuBar(false);
+  return menuBar;
+}
+
+QLabel *MainWindow::CreateTitle() {
+  QLabel *title = new QLabel("Hello World!", this);
+  title->setAlignment(Qt::AlignCenter);
+  title->setStyleSheet(QString("QLabel {"
+                               "  font-size: 28px;"
+                               "  font-weight: bold;"
+                               "  color: %1;"
+                               "}")
+                           .arg(m_textColor));
+  return title;
+}
+
+QLabel *MainWindow::CreateSubtitle() {
+  QLabel *subtitle = new QLabel("Est. 2026", this);
+  subtitle->setAlignment(Qt::AlignCenter);
+  subtitle->setStyleSheet(QString("QLabel {"
+                                  "  font-size: 11px;"
+                                  "  font-weight: normal;"
+                                  "  color: %1;"
+                                  "}")
+                              .arg(m_textColor));
+  return subtitle;
+}
+
+QLabel *MainWindow::CreateCenterImage() {
+  QLabel *image = new QLabel(this);
+  QPixmap pixmap(":/images/globe.png");
+  pixmap =
+      pixmap.scaled(250, 250, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+  image->setPixmap(pixmap);
+  image->setFixedSize(250, 250);
+  return image;
 }
--- MainWindow.h
+++ MainWindow.h
@@ -1,7 +1,9 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QLabel>
 #include <QMainWindow>
+#include <QString>
 
 class MainWindow : public QMainWindow {
   Q_OBJECT
@@ -10,7 +12,13 @@ public:
   MainWindow(bool isDarkMode, QWidget *parent = nullptr);
 
 private:
-  bool m_isDarkMode{false};
+  QMenuBar *CreateMenuBar();
+  QLabel *CreateTitle();
+  QLabel *CreateSubtitle();
+  QLabel *CreateCenterImage();
+
+  bool m_isDarkMode;
+  QString m_textColor; // based on m_isDarkMode
 };
 
 #endif // MAINWINDOW_H
